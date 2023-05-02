ad1b2f3aebf590fe59f1686d15c72d1576d4c123
Samuel Jasek <jasek.samuel0@gmail.com>
2023-05-02 18:14:08 +0200

    - Make the search algorithm better

--- server/controllers/searchController.js
+++ server/controllers/searchController.js
@@ -5,7 +5,18 @@ const search = async(req, res) => {
     try {
         const searchQuery = req.query.query;
         if(!searchQuery) return res.sendStatus(400);
-        const fetchDB = await pool.query("SELECT * FROM songs WHERE (lower(title) LIKE lower($1)) OR (lower(artist_name) LIKE lower($1)) LIMIT 100",["%" + searchQuery + "%"]);
+        let fetchDB;
+        if(searchQuery.includes(" ")) { 
+            const splitQuery = splitString(searchQuery);
+            // [0] -> title (SNAD HOPEFULLY)
+            // [1] -> artist (PLEASE)
+            fetchDB = await pool.query("SELECT * FROM songs WHERE (lower(title) LIKE lower($1) AND lower(artist_name) LIKE (lower($2))) LIMIT 100",["%" + splitQuery[0] + "%", "%" + splitQuery[1] + "%"]);
+            if(fetchDB.rows.length === 0) {
+                fetchDB = await pool.query("SELECT * FROM songs WHERE (lower(title) LIKE lower($1)) OR (lower(artist_name) LIKE lower($1)) LIMIT 100",["%" + searchQuery + "%"]);
+            } 
+         } else {
+            fetchDB = await pool.query("SELECT * FROM songs WHERE (lower(title) LIKE lower($1)) OR (lower(artist_name) LIKE lower($1)) LIMIT 100",["%" + searchQuery + "%"]);
+         }
         if(!fetchDB.rows) return res.sendStatus(401);
         res.json(fetchDB.rows);
     } catch (error) {
@@ -13,6 +24,16 @@ const search = async(req, res) => {
     }
 }
 
+function splitString(str) {
+    const lastSpaceIndex = str.lastIndexOf(' ');
+    if (lastSpaceIndex !== -1) { // if space is found
+      const firstSubstring = str.substring(0, lastSpaceIndex);
+      const secondSubstring = str.substring(lastSpaceIndex + 1);
+      return [firstSubstring, secondSubstring];
+    } else { // if no space is found
+      return [str];
+    }
+  }
 
 module.exports = {
     search,
